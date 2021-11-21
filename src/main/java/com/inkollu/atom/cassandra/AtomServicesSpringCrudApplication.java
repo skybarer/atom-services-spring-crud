09c7629fbca596e0e80e8f0c98b6bca0dec122fe
skybarer <akashdhar.apssdc@gmail.com>
2021-11-21 21:25:31 +0530

    first commit

---
+++ src/main/java/com/inkollu/atom/cassandra/AtomServicesSpringCrudApplication.java
@@ -0,0 +1,13 @@
+package com.inkollu.atom.cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomServicesSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomServicesSpringCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inkollu/atom/cassandra/AtomServicesSpringCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inkollu.atom.cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomServicesSpringCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

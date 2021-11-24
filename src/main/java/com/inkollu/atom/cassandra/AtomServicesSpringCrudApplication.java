38eab4c1e0bfa2a34bd26fef63e11473ec340a57
skybarer <akashdhar.apssdc@gmail.com>
2021-11-24 22:52:38 +0530

    keyspace property modified

--- src/main/java/com/inkollu/atom/cassandra/AtomServicesSpringCrudApplication.java
+++ src/main/java/com/inkollu/atom/cassandra/AtomServicesSpringCrudApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.inkollu.atom.cassandra", "com.akash.atom"})
+@ComponentScan(basePackages = {"com.inkollu.atom.cassandra"})
+//@ComponentScan(basePackages = {"com.inkollu.atom.cassandra", "com.akash.atom"})
 public class AtomServicesSpringCrudApplication implements CommandLineRunner {
     @Autowired
     public ApplicationContext applicationContext;
@@ -23,6 +25,6 @@ public class AtomServicesSpringCrudApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        new CassandraAccess(applicationContext);
+//        new CassandraAccess(applicationContext);
     }
 }
